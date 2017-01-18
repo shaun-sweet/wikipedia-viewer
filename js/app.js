7353ec0ae1f0d1f1ed46608c9e151b3bf185df1f
shaun-sweet <shaun@shaunsweet.com>
2017-01-18 12:52:15 -0700

    add color and css variables

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-  console.log("perf");
+  var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+  var cb = '&callback=JSON_CALLBACK';
+  
 });
