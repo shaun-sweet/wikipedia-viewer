247ef39daaf184825a681548cb41ef693ab7dc55
shaun-sweet <shaun@shaunsweet.com>
2017-01-18 14:11:15 -0700

    UI looking better, sending up to web server to test AJAX

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
+  var magnifyingGlass = $("#mag-glass");
   var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
   var cb = '&callback=JSON_CALLBACK';
-  
+  var input;
+
+  magGlassListener();
+
+  function magGlassListener(){
+      magnifyingGlass.on('click', function() {
+      magnifyingGlass.slideUp();
+      addSearchBar();
+    });
+  }
+
+  function addSearchBar() {
+    $('body').append("<input id='search-query' type='text' placeholder='Query Parameters...'>");
+    $("#search-query").fadeIn("slow");
+    input = $("#search-query");
+    input.on('keyup', function(e) {
+      if (e.keyCode == 13) {
+        wikiAjaxCall();
+      }
+    })
+  }
+
+  function wikiAjaxCall() {
+    var queryParams = $('input').val()
+    $.getJSON(api + queryParams + cb, function(res) {
+      console.log(res);
+    })
+  }
 });
