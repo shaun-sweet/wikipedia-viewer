c4f80986f01528d8e42951e5f257da7d7e6bdbf0
shaun-sweet <shaun@shaunsweet.com>
2017-01-18 17:12:53 -0700

    MVP commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   var magnifyingGlass = $("#mag-glass");
   var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
   var cb = '&callback=JSON_CALLBACK';
+  var pageURL = 'https://en.wikipedia.org/?curid=';
   var input;
 
   magGlassListener();
@@ -11,30 +12,46 @@ $(document).ready(function() {
       magnifyingGlass.slideUp();
       addSearchBar();
     });
-  }
+  };
 
   function addSearchBar() {
     $('body').append("<input id='search-query' type='text' placeholder='Query Parameters...'>");
     $("#search-query").fadeIn("slow");
     input = $("#search-query");
+    input.focus();
     input.on('keyup', function(e) {
       if (e.keyCode == 13) {
+        $(".wiki-entries").remove();
         wikiAjaxCall();
       }
     })
-  }
+  };
 
   function wikiAjaxCall() {
+    var queryParams = encodeURIComponent($('input').val());
     var url = api + queryParams + cb;
-    var queryParams = $('input').val()
+
     $.ajax({
       dataType: "jsonp",
       url: url
     }).done(function(res) {
       console.log(res);
+      for (var entry in res.query.pages) {
+        var title = res.query.pages[entry].title;
+        var body = res.query.pages[entry].extract;
+        var pageID = res.query.pages[entry].pageid;
+        appendWikiEntries(title, body, pageID);
+      }
+
     });
-    // $.getJSON(api + queryParams + cb, function(res) {
-    //
-    // })
-  }
+  };
+
+  function appendWikiEntries(title, body, pageID) {
+    $(`<div class='wiki-entries'>
+    <h3>`+title+`</h3>
+    <p>`+body+`</p>
+    </div>`).hide().appendTo("body").fadeIn().on('click', function() {
+      window.open(pageURL + pageID);
+    });
+  };
 });
